9ba780267bdd9f222292f56a8baae8ef0aa6341b
sociablerobot10 <19kam.sha@gmail.com>
2024-09-27 22:40:29 -0400

    rename routers to routes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const { PORT = 3001 } = process.env;
-const mainRouter = require("./routers/index");
+const mainRouter = require("./routes/index");
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").then(() => {
   console.log("connected");
 });
