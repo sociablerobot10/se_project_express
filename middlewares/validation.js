4abaf14d51cd75d9468a5643c4c843a60560b1a7
sociablerobot10 <19kam.sha@gmail.com>
2025-01-08 00:07:31 -0500

    validate params of api

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -8,6 +8,13 @@ const validateUrl = (value, helpers) => {
   return helpers.error("string.uri");
 };
 
+const validateEmail = (value, helpers) => {
+  if (validator.isEmail(value)) {
+    return value;
+  }
+  return helpers.error("string.email");
+};
+
 const validateClothingItem = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30).messages({
@@ -19,4 +26,26 @@ const validateClothingItem = celebrate({
   }),
 });
 
-module.exports = { validateClothingItem };
+const validateUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": "The name of the item is too short",
+      "string.max": "The name of the item is too long",
+    }),
+    email: Joi.string().required().custom(validateEmail),
+    password: Joi.string().required(),
+  }),
+});
+
+const validateUserID = celebrate({
+  params: Joi.object().keys({
+    id: Joi.hex().required().length(24),
+  }),
+});
+const validateclothingItemID = celebrate({
+  params: Joi.object().keys({
+    id: Joi.hex().required().length(24),
+  }),
+});
+
+module.exports = { validateClothingItem, validateUser };
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
-const { getCurrentUser, updateUser } = require("../controllers/users");
+const {
+  getCurrentUser,
+  updateUser,
+  createUser,
+} = require("../controllers/users");
 // all routes start with /users
+const { validateUser } = require("../middlewares/validation");
 
 const authorizeUser = require("../middlewares/auth");
 
 // router.get("/", authorizeUser, getUsers);
-// router.post("/", authorizeUser, createUser);
+router.post("/", validateUser, authorizeUser, createUser);
 router.get("/me", authorizeUser, getCurrentUser);
 router.patch("/me", authorizeUser, updateUser);
 // router.get("/:userId", authorizeUser, getUserById);
